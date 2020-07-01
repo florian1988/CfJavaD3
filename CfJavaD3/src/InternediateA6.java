e82750901ddb5e13e709e5a65609dbe472728d43
florian <flo.moerzinger@gmx.at>
2020-07-01 14:49:06 +0200

    Example 6

---
+++ CfJavaD3/src/InternediateA6.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class InternediateA6 {
+
+
+    public static void A6() {
+
+        int InputNumber;
+
+        Scanner ScannerNumber = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+
+        InputNumber = ScannerNumber.nextInt();
+
+        //calculation for fizz and buzz
+        int Outcome1 = InputNumber % 3;
+        int Outcome2 = InputNumber % 5;
+
+
+        /*
+        if statement with a nested if statement for determinate
+        fizz buzz or  FizzBuzz
+         */
+        if (Outcome1 == 0){
+
+            if( Outcome2 == 0){
+                System.out.println("FizzBuzz!!!");
+            } else {
+                System.out.println("FIZZ");
+            }
+
+        }else if (Outcome2 == 0) {
+
+            if (Outcome1 == 0) {
+                System.out.println("FizzBuzz!!!");
+            } else {
+                System.out.println("BUZZ");
+            }
+
+        } else {
+            System.out.println(InputNumber);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        A6();
+    }
+
+}
